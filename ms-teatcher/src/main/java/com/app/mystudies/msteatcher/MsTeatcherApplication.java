9a0c2a7646b204e367d2efede42b6ab874167198
Rodrigo <rodmatheus99@gmail.com>
2022-03-27 15:04:36 -0300

    add micro servico de professores

---
+++ ms-teatcher/src/main/java/com/app/mystudies/msteatcher/MsTeatcherApplication.java
@@ -0,0 +1,13 @@
+package com.app.mystudies.msteatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTeatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTeatcherApplication.class, args);
+	}
+
+}
