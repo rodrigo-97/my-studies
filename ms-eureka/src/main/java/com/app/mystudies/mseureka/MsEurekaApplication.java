9bfc64b33bb9e89a59782761af501e89d9024c2b
Rodrigo <rodmatheus99@gmail.com>
2022-03-28 22:42:20 -0300

    add eureka server

---
+++ ms-eureka/src/main/java/com/app/mystudies/mseureka/MsEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.app.mystudies.mseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaApplication.class, args);
+	}
+
+}
---
+++ ms-eureka/src/test/java/com/app/mystudies/mseureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mystudies.mseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
